f9067cb26f260020df8c5f792477536080548829
Chris Stead <cmstead@gmail.com>
2020-02-13 11:39:16 -0800

    Fresh build of workshop resources

---
+++ scripts/build-release.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const { execSync } = require('child_process');
+
+const [,,versionType] = process.argv;
+
+if(!versionType) {
+    throw new Error('Please specify a version type (major, minor, patch)');
+}
+
+function packageSnippets() {
+    const snippetDir = path.join(
+        process.cwd(),
+        'workshop-assets',
+        'tdd-workshop-snippets'
+    );
+
+    execSync(
+        'vsce package',
+        { 
+            cwd: snippetDir,
+            stdio: 'inherit'
+        }
+    );
+}
+
+function versionRepository() {
+    execSync('git add --all');
+    execSync('git commit -m "Fresh build of workshop resources"');
+    execSync('npm version ' + versionType, { stdio: 'inherit' });
+}
+
+packageSnippets();
+versionRepository();
