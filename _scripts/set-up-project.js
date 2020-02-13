d6a3a688ae2d70f99732d40cf52a6e6e8264bf74
Chris Stead <cmstead@gmail.com>
2020-02-13 15:25:11 -0800

    Fresh build of workshop resources

--- _scripts/set-up-project.js
+++ _scripts/set-up-project.js
@@ -1,5 +1,9 @@
 const { execSync } = require('child_process');
 
+execSync('npm install yo@latest -g',{
+    stdio: "inherit"
+});
+
 execSync('code --install-extension ./tdd-workshop-snippets.vsix', {
     cwd: "./_workshop-assets/tdd-workshop-snippets",
     stdio: "inherit"
@@ -14,6 +18,8 @@ const devBranchExists = !execSync(
     'git show-ref --verify refs/heads/dev',
     { encoding: 'utf8' })
     .startsWith('fatal');
+
+execSync('git checkout master');
 
 if (devBranchExists) {
     execSync('git branch -D dev');
