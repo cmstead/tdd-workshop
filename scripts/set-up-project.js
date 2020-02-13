bd144f14e232108b36635ad35e6ec9d9b1ebe912
Chris Stead <cmstead@gmail.com>
2020-02-13 15:08:38 -0800

    Fresh build of workshop resources

--- scripts/set-up-project.js
+++ scripts/set-up-project.js
@@ -1,2 +1,11 @@
 const { execSync } = require('child_process');
 
+execSync('code --install-extension ./tdd-workshop-snippets.vsix', {
+    cwd: "./workshop-assets/tdd-workshop-snippets",
+    stdio: "inherit"
+});
+
+execSync('npm link ./', {
+    cwd: "./workshop-assets/generator-ts-tdd",
+    stdio: "inherit"
+});
