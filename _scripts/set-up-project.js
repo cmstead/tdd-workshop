6c2c01449c1319d70c6e81fb9160425d81744b5f
Chris Stead <cmstead@gmail.com>
2020-02-19 14:06:59 -0800

    Fresh build of workshop resources

--- _scripts/set-up-project.js
+++ _scripts/set-up-project.js
@@ -1,14 +1,23 @@
 const { execSync } = require('child_process');
 
-execSync('npm install yo@latest -g',{
+console.log('\nInstalling project API dependencies...');
+execSync('npm install', {
+    cwd: './_project-api',
+    stdio: 'inherit'
+})
+
+console.log('\nInstalling Yeoman...');
+execSync('npm install yo@latest -g', {
     stdio: "inherit"
 });
 
+console.log('\nInstalling VS Code snippets extension...');
 execSync('code --install-extension ./tdd-workshop-snippets.vsix', {
     cwd: "./_workshop-assets/tdd-workshop-snippets",
     stdio: "inherit"
 });
 
+console.log('\nLinking File generator...');
 execSync('npm link ./', {
     cwd: "./_workshop-assets/generator-ts-tdd",
     stdio: "inherit"
@@ -18,6 +27,8 @@ const devBranchExists = !execSync(
     'git show-ref --verify refs/heads/dev',
     { encoding: 'utf8' })
     .startsWith('fatal');
+
+console.log('\nPreparing git branches...');
 
 execSync('git checkout master');
 
